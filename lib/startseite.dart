a22676167b2168423200bfbc0181e71c336a5e75
Joseph Ikinda <josephikinda@gmail.com>
2022-01-30 17:06:54 +0100

    also done

--- lib/startseite.dart
+++ lib/startseite.dart
@@ -1,6 +1,10 @@
 import 'package:augmented_flutter/example/gesture_page.dart';
+import 'package:augmented_flutter/example/gesture_pagedeux.dart';
 import 'package:augmented_flutter/example/local_page.dart';
+import 'package:augmented_flutter/example/simple_ar.dart';
 import 'package:flutter/material.dart';
+
+import 'example/ar_screenshot.dart';
 
 class Accueil extends StatelessWidget {
   const Accueil({Key? key}) : super(key: key);
@@ -31,15 +35,56 @@ class Accueil extends StatelessWidget {
                 height: 150,
                 width: size.width * 0.45,
                 child: TextButton(
+                  child: const Text('Gesture 2'),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ObjectsOnPlanesWidget()));
+                  },
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                height: 150,
+                width: size.width * 0.45,
+                child: TextButton(
                   child: const Text('local gltf'),
                   onPressed: () {
                     Navigator.of(context).push(MaterialPageRoute(
                         builder: (context) => LocalAndWebObjectsWidget()));
                   },
                 ),
-              )
+              ),
+              SizedBox(
+                height: 150,
+                width: size.width * 0.45,
+                child: TextButton(
+                  child: const Text('Simple'),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => DebugOptionsWidget()));
+                  },
+                ),
+              ),
             ],
-          )
+          ),
+          Row(
+            children: [
+              SizedBox(
+                height: 150,
+                width: size.width * 0.45,
+                child: TextButton(
+                  child: const Text('ArScreenschot'),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ScreenshotWidget()));
+                  },
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
