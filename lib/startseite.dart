32dd84135e077ac8d5262e62420268e41276a806
Joseph Ikinda <josephikinda@gmail.com>
2022-01-30 13:13:01 +0100

    first commit

---
+++ lib/startseite.dart
@@ -0,0 +1,35 @@
+import 'package:augmented_flutter/example/gesture_page.dart';
+import 'package:flutter/material.dart';
+
+class Accueil extends StatelessWidget {
+  const Accueil({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ar experience'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                height: 50,
+                width: size.width * 0.45,
+                child: TextButton(
+                  child: const Text('Gesture'),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ObjectGesturesWidget()));
+                  },
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
